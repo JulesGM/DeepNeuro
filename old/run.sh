4097a65c85154125a6b5d15b7116add7df0f59c9
Jules Gagnon-Marchand <jules_gagnonm@hotmail.com>
2016-07-25 04:06:06 -0400

    sunday

--- old/run.sh
+++
@@ -1,4 +0,0 @@
-# !/usr/bin/env bash
-CODE='ipython ./debut_conv.py 2>&1 | grep -v "naming conventions" | grep -v "raw_data = Raw(file_path)$"'
-echo $CODE
-eval "$CODE"
--- old/time.sh
+++
@@ -1,4 +0,0 @@
-# !/usr/bin/env bash
-CODE='time ipython ./debut_conv.py 2>&1 | grep -v "naming conventions" | grep -v "data = fid.read(int(end - start))$"'
-echo $CODE
-eval "$CODE"
